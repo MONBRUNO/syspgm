2c3b873f60403bcf1c87316ddf072ba14ca22391
MONBRUNO <129732862+MONBRUNO@users.noreply.github.com>
2025-05-16 09:19:02 +0900

    Add files via upload

---
+++ 0502/0502/argument.c
@@ -0,0 +1,12 @@
+// 파일 이름: print_args.c
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("인자의 개수: %d\n", argc);
+
+    for (int i = 0; i < argc; i++) {
+        printf("argv[%d]: %s\n", i, argv[i]);
+    }
+
+    return 0;
+}
